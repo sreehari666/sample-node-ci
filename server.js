0338e61a9071bc8df4099dc1c8c8a85c1696a894
Sreehari <sreeharivr855@gmail.com>
2025-12-02 23:18:25 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("🚀 CI/CD Pipeline working via Jenkins on WSL2!");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
